52bfd1d7f5d8f26b875a4d5d5f1aaf6ce3947835
Ján Dovjak <dovjak.jan@gmail.com>
2024-05-01 13:22:58 +0200

    Overlaying lines merged and example file updated

--- kml.py
+++ kml.py
@@ -5,7 +5,8 @@ import ipaddress
 import random
 import sys
 
-HEADER = '<?xml version="1.0" encoding="UTF-8"?>\n<kml xmlns="http://www.opengis.net/kml/2.3">\n<Document>\n'
+HEADER = '<?xml version="1.0" encoding="UTF-8"?>\n\
+    <kml xmlns="http://www.opengis.net/kml/2.3">\n<Document>\n'
 FOOTER = '</Document>\n</kml>\n'
 
 
@@ -13,6 +14,10 @@ class Position:
     def __init__(self, longtitude, latitude):
         self.longtitude = longtitude
         self.latitude = latitude
+
+    def __str__(self):
+        return str(self.longtitude) + ','\
+            + str(self.latitude)
 
 
 class IP:
@@ -42,15 +47,36 @@ class IP:
     def __eq__(self, other):
         if not isinstance(other, IP):
             return False
-        return self.address == other.address 
+        return self.address == other.address
+
+    def __str__(self):
+        return str(self.address)
+
+    def get_position(self):
+        return self.position
 
 
 class Route:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
+        self.name = str(destination)
         self.color = hex(random.randint(0, 16777215))
         self.style = str(destination.address).replace('.', '_')
+
+    def get_line(self):
+        return str(self.source.get_position()) + ';'\
+            + str(self.destination.get_position())
+
+    def get_line_kml(self):
+        return str(self.source.get_position()) + '\n'\
+            + str(self.destination.get_position())
+
+    def get_name(self):
+        return self.name
+
+    def append_name(self, name):
+        self.name += '; ' + name
 
     def __str__(self):
         return f"<Style id=\"{self.style}\">\n\
@@ -60,12 +86,12 @@ class Route:
 </LineStyle>\n\
 </Style>\n\
 <Placemark>\n\
-<name>{self.destination.address}</name>\n\
+<name>{self.name}</name>\n\
 <extrude>1</extrude>\n\
 <tessellate>1</tessellate>\n\
 <styleUrl>#{self.style}</styleUrl>\n\
 <LineString>\n\
-<coordinates>{self.destination.position.longtitude},{self.destination.position.latitude}\n{self.source.position.longtitude},{self.source.position.latitude}</coordinates>\n\
+<coordinates>{self.get_line_kml()}</coordinates>\n\
 </LineString>\n\
 </Placemark>\n\
 "
@@ -124,10 +150,28 @@ def create_routes(ip_addresses, home_position):
     return routes
 
 
+def merge_routes(routes):
+    merged_routes = []
+    routes_to_merge = {}
+    for route in routes:
+        line = route.get_line()
+        if line in routes_to_merge:
+            routes_to_merge[line].append(route)
+        else:
+            routes_to_merge[line] = [route]
+    for routes_on_line in routes_to_merge.values():
+        merged_route = routes_on_line[0]
+        for route in routes_on_line[1:]:
+            merged_route.append_name(route.get_name())
+        merged_routes.append(merged_route)
+    return merged_routes
+
+
 def generate_kml(input_file, home_longtitude, home_latitude):
     home_position = Position(home_longtitude, home_latitude)
     ip_addresses = filter_ip_addresses(input_file)
     routes = create_routes(ip_addresses, home_position)
+    routes = merge_routes(routes)
     content = HEADER
     for route in routes:
         content += "\n" + str(route)
@@ -143,8 +187,8 @@ def main():
     output_file = 'output.kml'
     if len(sys.argv) > 2:
         output_file = sys.argv[2]
-    home_longtitude = 16
-    home_latitude = 49
+    home_longtitude = 16.3662
+    home_latitude = 48.2049
     content = generate_kml(input_file, home_longtitude, home_latitude)
     with open(output_file, 'w') as file:
         file.write(content)
